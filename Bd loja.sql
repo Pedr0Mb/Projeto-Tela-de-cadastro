fb186938a534be9d6a39b86a127e9a8139141579
Pedr0Mb <160172174+Pedr0Mb@users.noreply.github.com>
2024-08-07 13:45:51 -0300

    Add files via upload
    Script do banco de dados

---
+++ Bd loja.sql
@@ -0,0 +1,31 @@
+Create database bd_loja;
+use bd_loja;
+
+create table tb_funcionario(
+id_funcionario int primary key auto_increment,
+nm_funcionario varchar(45) not null,
+nm_sobrenome varchar(45) not null,
+dt_nascimento date not null,
+ds_cargo varchar(30) not null,
+nr_salario decimal not null,
+dt_admissao date not null,
+nm_senha text not null
+);
+
+create table tb_cliente(
+id_cliente int primary key auto_increment,
+nm_cliente varchar(45) not null,
+nm_sobrenome varchar(45) not null,
+nm_email varchar(50) not null,
+nr_telefone varchar(11) not null,
+nm_endereco varchar(50) not null
+);
+
+create table tb_fornecedor(
+id_fornecedor int primary key auto_increment,
+nm_fornecedor varchar(45) not null,
+nm_contato varchar(45) not null,
+nm_email varchar(50) not null,
+nr_telefone varchar(11) not null,
+nm_endereco varchar(50) not null
+);
