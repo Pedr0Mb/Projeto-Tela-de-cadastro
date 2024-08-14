809999dd32003fcb7b670613332318b19fcfe635
Pedr0Mb <160172174+Pedr0Mb@users.noreply.github.com>
2024-08-14 14:18:31 -0300

    Add files via upload

---
+++ LoginCliente.js
@@ -0,0 +1,8 @@
+function Limpar(){
+    document.getElementById('Nome').value = ''
+    document.getElementById('Sobrenome').value = ''
+    document.getElementById('Email').value = ''
+    document.getElementById('Telefone').value = ''
+    document.getElementById('Endereco').value = ''
+}
+ 
