4e01685fdd8bb7059efbd4c540bf5e4ede72b1be
Pedr0Mb <160172174+Pedr0Mb@users.noreply.github.com>
2024-09-11 16:55:06 -0300

    Delete LoginFornecedor.js

--- LoginFornecedor.js
+++
@@ -1,8 +0,0 @@
-function Limpar(){
-    document.getElementById('Nome').value = ''
-    document.getElementById('Contato').value = ''
-    document.getElementById('Email').value = ''
-    document.getElementById('Telefone').value = ''
-    document.getElementById('Endereco').value = ''
-}
- 
